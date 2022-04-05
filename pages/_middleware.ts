b80c8d189445e15be6ac4d862421e3c38e58303a
Rafael Brun Golin <rafaelbrungolin@gmail.com>
2022-04-05 10:28:44 -0300

    feat(MainPage): implement and styling initial pages

---
+++ pages/_middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse, NextRequest } from "next/server";
+
+const instaUrl =
+  "https://api.instagram.com/oauth/authorize" +
+  "?client_id=1281631838991518" +
+  "&redirect_uri=http://localhost:3000/main/" +
+  "&scope=user_profile" +
+  "&response_type=code";
+
+export async function middleware(req: any, ev: any) {
+  const { pathname } = req.nextUrl;
+  if (pathname == "/insta-api") {
+    return NextResponse.redirect(instaUrl);
+  }
+  return NextResponse.next();
+}
---
+++ pages/insta-api/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const InstaApi: NextPage = () => {
+  return (
+    <div>
+      <h1>HELLO</h1>
+    </div>
+  );
+};
+
+export default InstaApi;
