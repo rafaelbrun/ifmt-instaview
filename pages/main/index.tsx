cbfdf8150370ff6630f4d35bc97cf490dd44f63b
Rafael Brun Golin <rafaelbrungolin@gmail.com>
2022-04-05 18:07:28 -0300

    feat(MainPage): style main page

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,9 +1,28 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { BsFillFileEarmarkCheckFill } from "react-icons/bs";
+import styles from "../../styles/Main.module.css";
 
 const Main: NextPage = () => {
+  const router = useRouter();  
+  const accessToken = router.query.code as string;
+  console.log(accessToken);
+  
   return (
-    <div>
-      <h1>HELLO</h1>
+    <div className={styles.container}>
+      <h4>Aqui está seu token de acesso</h4>
+      <div className={styles.iconContainer}>
+        <input readOnly className={styles.inputText} value={accessToken} />
+        <div
+          className={styles.hovertext}
+          data-hover="Copiar token"
+          onClick={() => {
+            navigator.clipboard.writeText(accessToken);
+          }}
+        >
+          <BsFillFileEarmarkCheckFill className={styles.icon} />
+        </div>
+      </div>
     </div>
   );
 };
