ac0c705a7810b3945d34a63ca718428290131ee1
andrevks <italiabk@gmail.com>
2022-05-13 08:38:10 -0400

    build: access_code from instagram

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,93 +1,80 @@
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
-import { BsFillFileEarmarkCheckFill } from "react-icons/bs";
+import type {NextPage}
+from "next";
+import {BsFillFileEarmarkCheckFill} from "react-icons/bs";
 import styles from "../../styles/Main.module.css";
 
-const Main: NextPage = ({code}: any) => {
-  const accessToken = code;
+const Main: NextPage = ({accessToken} : any) => {
 
   return (
-    <div className={styles.container}>
-      <h4>Aqui está seu token de acesso adsfsda</h4>
-      <div className={styles.iconContainer}>
-        <input readOnly className={styles.inputText} value={accessToken} />
-        <div
-          className={styles.hovertext}
+    <div className={
+      styles.container
+    }>
+      <h4>Aqui está seu token de acesso</h4>
+      <div className={
+        styles.iconContainer
+      }>
+        <input readOnly
+          className={
+            styles.inputText
+          }
+          value={accessToken}/>
+        <div className={
+            styles.hovertext
+          }
           data-hover="Copiar token"
-          onClick={() => {
-            navigator.clipboard.writeText(accessToken);
-          }}
-        >
-          <BsFillFileEarmarkCheckFill className={styles.icon} />
+          onClick={
+            () => {
+              navigator.clipboard.writeText(accessToken);
+            }
+        }>
+          <BsFillFileEarmarkCheckFill className={
+            styles.icon
+          }/>
         </div>
       </div>
     </div>
   );
 };
 
-export async function getServerSideProps(context: any) {
-  const { code } = context.query;
+export async function getServerSideProps(context : any) {
+  const {code} = context.query;
+
   const axios = require('axios');
 
-  
-  const FormData = require('form-data');
-  const form = new FormData();
-  form.append('client_id', 1281631838991518)
-  form.append('code', code );
-  form.append('client_secret', '406c81da7ddf02a38b192ecec5a11a29');
-  form.append('redirect_uri', 'https://ifmt-instaview.vercel.app/main/');
-  form.append('grant_type', 'authorization_code' );
-  // form.append('Content-Type', 'application/x-www-form-urlencoded')
+  async function getUserTokenByCode() {
+    try {
+      const params = new URLSearchParams({foo: 'bar'});
+      params.append('extraparam', 'value');
+      params.append('client_id', '1281631838991518');
+      params.append('client_secret', '406c81da7ddf02a38b192ecec5a11a29');
+      params.append('redirect_uri', 'https://ifmt-instaview.vercel.app/main/');
+      params.append('grant_type', 'authorization_code');
+      params.append('code', code);
 
-  axios.post( 'http://api.instagram.com/oauth/access_token', {
-    data: form,
-    headers: form.getHeaders()
+      const response = await axios.post('https://api.instagram.com/oauth/access_token', params);
+      console.log(response.data);
+      return response;
 
+    } catch (e) {
+      console.log(e);
+    }
   }
-  ).then(function (response: any) {
-    console.log(response.data.form);
-  }).catch(function (error: any) {
-    console.log(error);
-  });
 
-
-
-//   console.log(form);
-  
-
-//   const http = require('http');
-
-// const request = http.request({
-//   method: 'post',
-//   host: 'http://api.instagram.com',
-//   path: '/oauth/access_token',
-//   headers: form.getHeaders()
-// });
-
-// form.pipe(request);
-
-// request.on('response', function(res: any) {
-//   console.log(res.statusCode);
-// });
-
-
+  const responseData = await getUserTokenByCode();
 
   // const { app, database } = require('../../firebaseConfig');
   // const{ collection, addDoc } = require('firebase/firestore');
 
-
-
   // const dbInstance = collection(database, 'tokens');
 
-  //   addDoc(dbInstance, {
+  // addDoc(dbInstance, {
   //       username: 'teste',
   //       token: code
-  //   })
+  // })
 
   return {
     props: {
-      code,
-    },
-  };
+      accessToken: responseData? responseData.data.access_token : 'O código de autorização foi usado! Tente novamente.'
+    }};
 }
 export default Main;
