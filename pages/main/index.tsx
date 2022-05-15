01d703dccd4f56d3c3596c99a641f2832b22129f
andrevks <italiabk@gmail.com>
2022-05-15 16:08:31 -0400

    fix: comment on get for user fields

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -68,7 +68,7 @@ export async function getServerSideProps(context: any) {
       `&access_token=${responseData?.data.access_token}`
   );
 
-  const username = await axios.get(`https://graph.instagram.com/me?fields=username&access_token=longDurationToken?.data.access_token}`)
+  // const username = await axios.get(`https://graph.instagram.com/me?fields=username&access_token=longDurationToken?.data.access_token}`)
 
   // const FiftyNineDaysFromNow = new Date(new Date().setDate(new Date().getDate() + 59)).toLocaleDateString();
   
